686d752c538f46f472d07f1729b18003d25539a7
Alejandro Figueroa <alejandro@ideasftw.com>
2014-11-22 15:35:08 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+## Introduction
+Use a raspberry pi to determine whether I should walk, run, or not even bother to try to catch the next bus.
+
+## Constraints
+- write everything in javascript/node.js
+- use gps to determine current locations
+- use nextbus api to determine bus predictions
+- must be able to choose between inbound and outbound
+- must be able to choose between multiple bus stops
+- must control a set of LEDs to determine actions
+  - green = walk
+  - yellow = run
+  - red = don't bother
+
+## Los Links
+[Introduction | Adafruit Ultimate GPS on the Raspberry Pi | Adafruit Learning System](https://learn.adafruit.com/adafruit-ultimate-gps-on-the-raspberry-pi/introduction)
---
+++ busTrafficLights.js
@@ -0,0 +1,51 @@
+var restbus  = require('restbus'),
+    http     = require('http'),
+    //bancroft = require('bancroft'),
+    //i2c      = require('i2c'),
+    //gpio     = require('pi-gpio'),
+    port     = '3535',
+    lon      = '-122.471005',
+    lat      = '37.765547';
+
+function buildPath(lat,lon) {
+  return '/locations/'+lat+','+lon+'/predictions'
+}
+
+restbus.listen(port, function() {
+  var path = buildPath(lat,lon),
+      options = {
+        hostname: 'localhost',
+        port: port,
+        path: path
+      };
+
+  http.get(options, function(res) {
+    res.setEncoding('utf8');
+    res.on("data", function(body) {
+      var data = JSON.parse(body);
+
+      for (var predictions in data) {
+        var prediction = data[predictions],
+            tab        = '\t',
+            agency     = prediction.agency.title,
+            route      = prediction.route.title,
+            stop       = prediction.stop.title,
+            distance   = prediction.stop.distance;
+            
+        for (var values in prediction.values) {
+          var value     = prediction.values[values],
+              direction = value.direction.title,
+              minutes   = value.minutes,
+              debug     = agency+tab+route+tab+stop+tab+distance+tab+direction
+                        +tab+minutes;
+
+          console.log(debug);
+        };
+      };
+    });
+  }).on('error', function(e) {
+    console.log("Got error: " + e.message);
+  });
+
+  //process.exit(0);
+});
